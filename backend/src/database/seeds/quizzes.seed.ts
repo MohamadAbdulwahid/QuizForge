ad9ea4c32061d48af50d5ff33267f8efe102ab92
Nishan277 <n.chhantyal@students.htl-leonding.ac.at>
2026-02-09 23:55:08 +0100

    skeleton of quizzes.seed.ts and user.seed.ts

---
+++ backend/src/database/seeds/quizzes.seed.ts
@@ -0,0 +1,7 @@
+import type {SeedFunction} from './seed.ts'
+import { faker } from '@faker-js/faker';
+
+export const seedQuizzes: SeedFunction = async () => {
+    return "not implemented";
+}
+
---
+++ backend/src/database/seeds/user.seed.ts
@@ -0,0 +1,6 @@
+import type {SeedFunction} from './seed.ts'
+import { faker } from '@faker-js/faker';
+
+export const seedUsers: SeedFunction = async () => {
+    return "not implemented";
+}
